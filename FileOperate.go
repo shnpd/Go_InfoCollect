5b3e68cd43242b7b16f89410c06b93bc6bf6218e
PolarDay <61319465+shn-1@users.noreply.github.com>
2022-02-27 14:47:00 +0800

    Update FileOperate.go

--- FileOperate.go
+++ FileOperate.go
@@ -6,7 +6,9 @@ func WriteFile(file string, jsonByte []byte) {
 	fp, err := os.OpenFile(file, os.O_RDWR, 0666)
 	if err != nil && os.IsNotExist(err) {
 		os.Create(file)
+ 		fp, err = os.OpenFile(file, os.O_RDWR, 0666)
 	}
+	
 	defer fp.Close()
 	_, err = fp.Write(jsonByte)
 	if err != nil {
